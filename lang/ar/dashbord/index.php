aec3e8eeda543f5b5d5695d8299105a0db30ca91
anasenadire <enadireahmed@gmail.com>
2023-01-30 21:07:37 +0000

    create the lang files of the 'index|home' page

---
+++ lang/ar/dashbord/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+return [
+    'products_store' => 'المخزن',
+    'clients' => 'العملاء',
+    'suppliers' => 'الموردين',
+    'sales' => 'المبيعات',
+    'pricing' => 'التسعيرات',
+    'purchases' => 'المشتريات',
+    'expences' => 'المصروفات',
+    'reports' => 'التقارير',
+    'employees' => 'الموظفين', 
+];
---
+++ lang/en/dashbord/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+return [
+    'products_store' => 'Products Store',
+    'clients' => 'Clients',
+    'suppliers' => 'Suppliers',
+    'sales' => 'Sales',
+    'pricing' => 'Pricing',
+    'purchases' => 'Purchases',
+    'expences' => 'Expences',
+    'reports' => 'repots',
+    'employees' => 'Employees',
+];
