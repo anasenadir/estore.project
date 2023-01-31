f8d526919d7f1ad0bf4b31b241d03631498a3a57
anasenadire <enadireahmed@gmail.com>
2023-01-31 19:07:17 +0000

    create the expenses category model and lang folder, and done with (index , create) lang files

---
+++ lang/ar/expensesCategories/create.php
@@ -0,0 +1,33 @@
+<?php 
+
+return [
+    'page_title'                    => 'مصروف جديد',
+    'E_category_title'              => 'مصروف جديد',
+
+    'E_category_name'               => 'اسم المصروف',
+    'E_category_minimum_amount'     => 'المبلغ الأدنى للمصروف',
+
+
+    'clients'           => 'العميل',
+    'products'          => 'المنتجات',
+    'product_name'      => 'إسم المنتج',
+    'quantity'          => 'الكمية المرادة',
+    'sele_price'        => 'سعر المنتج', 
+    'controle'          => 'التحكم',
+    'save_btn'          => 'حفظ',
+
+
+    
+    // messages
+    // 'products_are_required'     => 'يجب عليك إختيار منتجات لكي تتم عملية التسعير',
+
+    'creation_success_message'  => 'تم إنشاء الصنف بنجاح',
+    'creation_error_message'    => 'لقد حصل خطأ في إنشاء الصنف',
+
+    // 'clents_error'              => 'يجب عليك إختيار الزبون ',
+    // 'products_error'            => 'يجب عليك تحديد المنتجات التي تريد',
+
+    // deleting messages
+    'deleting_success_message'  => 'لقد تم حذف التسعير بنجاح', 
+    'deleting_error_message'    => 'لقد حصل خطأ في  حذف التسعير' ,
+];
---
+++ lang/ar/expensesCategories/default.php
@@ -0,0 +1,26 @@
+<?php 
+return [
+    "page_title"                        => "أنواع المصروفات",
+    'E_category_title'                  => "أنواع المصروفات" ,
+    "add_new_E_category"                => "إنشاء صنف جديد" ,
+
+    'E_category_name'                   => 'اسم المصروف',
+    'E_category_minimum_amount'         => 'المبلغ الأدنى للمصروف',
+    'controle'                          => 'التحكم',
+
+    'edit_E_Category'                   => 'تعديل المصروف',
+    'delete_E_Category'                 => 'حذف المصروف',
+    //  =========================
+
+
+
+
+
+
+    
+    'download'                          => 'تحميل الوصل',
+    'convert_to_sele_contract'          => 'تحويل  إلى عقد بيع',
+    'see_the_expenses_category'         => 'رأية التسعير' , 
+    'send_expenses_category'            => 'ارسال عبر الايميل',
+    'delete_expenses_category_message'  => '',
+];
